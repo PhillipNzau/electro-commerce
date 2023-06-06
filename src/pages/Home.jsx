e37f7d060e7017d2e823077c3929a04319e2f8f8
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-06-06 16:55:34 +0300

    modified the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import Button from '../components/Button';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <>
+    <div>Home</div>
+    <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Products</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white p-6 shadow-md rounded-md transition duration-300 transform hover:-translate-y-1 hover:shadow-lg"
+            >
+              <img
+                src={product.image}
+                alt={product.productName}
+                className="w-full h-48 object-cover mb-4 rounded-md"
+              />
+              <div className="text-gray-800">
+                <h2 className="text-lg font-semibold mb-2">
+                  {product.productName}
+                </h2>
+                <p className="text-gray-600 line-clamp-3">
+                  {product.description}
+                </p>
+                <p className="text-lg font-semibold mt-4">${product.price}</p>
+                <Button
+                  text="Add to Cart"
+                  width={150}
+                  onClick={() => addToCart(product)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        
+      </div>
+    </>
+  )
 };
 
 export default Home;
