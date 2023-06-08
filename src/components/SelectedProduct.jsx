511a0eb665cc99857f951ee3a2bd2b8d01356a9d
StellahIreri <stellahmireri@gmail.com>
2023-06-08 10:12:07 +0300

    updated strokeWidth on selectedProducts component

--- src/components/SelectedProduct.jsx
+++ src/components/SelectedProduct.jsx
@@ -12,7 +12,7 @@ const SelectedProduct = () => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        strokeWidth={1.5}
+        strokeWidth={1.6}
         stroke="currentColor"
         className="w-6 h-6"
       >
