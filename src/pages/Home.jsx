fafe85732071e3141a86b1914117200463eb4b9c
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-06-08 22:43:34 +0300

    modified home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useState, useEffect} from "react";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import Button from "../components/Button";
+import Button from '../components/Button';
 
-const Home = () => {
+const Home = ({ onClick}) => {
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  const navigate = useNavigate();
-  const handleOnClick = (id) => {
-    navigate(`/${id}`, { state: { id } });
-  };
 
   useEffect(() => {
     fetch("http://localhost:3001/products")
@@ -27,44 +23,53 @@ const Home = () => {
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
+  const handleOnClick = (id) => {
+    navigate(`/${id}`, { state: { id } });
+  };
+
 
   return (
     <>
-      <div className=" mx-auto p-4">
+     <div className=" mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">Products</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {products.map((product) => (
             <div
               key={product.id}
-              onClick={() => handleOnClick(product.id)}
               className="bg-white p-6 shadow-md rounded-md transition duration-300 transform hover:-translate-y-1 hover:shadow-lg hover:cursor-pointer"
             >
-              <LazyLoadImage
-                effect="blur"
-                src={product.image}
-                alt={product.productName}
-                className="w-full h-48 object-cover mb-4 rounded-md"
-              />
-              <div className="text-gray-800">
-                <h2 className="text-lg font-semibold mb-2">
-                  {product.productName}
-                </h2>
-                <p className="text-gray-600 line-clamp-3">
-                  {product.description}
-                </p>
-                <p className="text-lg font-semibold mt-4">${product.price}</p>
-                <Button
-                  text="Add to Cart"
-                  width={150}
-                  onClick={() => addToCart(product)}
+              <div
+                onClick={() => {
+                  handleOnClick(product.id);
+                }}
+              >
+                <LazyLoadImage
+                  effect="blur"
+                  src={product.image}
+                  alt={product.productName}
+                  className="w-full h-48 object-cover mb-4 rounded-md"
                 />
+                <div className="text-gray-800">
+                  <h2 className="text-lg font-semibold mb-2">
+                    {product.productName}
+                  </h2>
+                  <p className="text-gray-600 line-clamp-3">
+                    {product.description}
+                  </p>
+                  <p className="text-lg font-semibold mt-4">${product.price}</p>
+                </div>
               </div>
+              <Button
+                text="Add to Cart"
+                width={150}
+                onClick={() => onClick(product.id)}
+              />
             </div>
           ))}
         </div>
       </div>
     </>
-  );
+  )
 };
 
 export default Home;
